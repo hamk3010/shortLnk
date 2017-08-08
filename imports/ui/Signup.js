446fcd4d13493d835653c26978dd28e63227f930
Hamza Khan <coolbuddy_96@hotmail.com>
2017-08-08 13:05:07 -0700

    Made a routes folder and cleaned up client/main.js and server/main.js

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -17,6 +17,11 @@ export default class Signup extends React.Component{
         Accounts.createUser({email: '', password: ''}, () => {
             let email = this.refs.email.value.trim();
             let password =  this.refs.password.value.trim();
+
+            if(password.length < 9){
+                return this.setState({error: 'Password must be more than 8 character long.'});
+            }
+
             Accounts.createUser({email, password: password}, (err) => {
                 if(err){
                     this.setState({error: err.reason});
@@ -40,7 +45,7 @@ export default class Signup extends React.Component{
                 {this.state.error ? <p>{this.state.error}</p> : undefined}
 
 
-                <form onSubmit= {this.onSubmit.bind(this)}>
+                <form onSubmit= {this.onSubmit.bind(this)} noValidate>
                     <input type= "email" ref= "email" name="email" placeholder="Email"/>
                     <input type= "password" ref= "password" name= "password" placeholder= "Password"/>
                     <button>Create Account</button>
--- server/main.js
+++ server/main.js
@@ -1,56 +1,7 @@
 import { Meteor } from 'meteor/meteor';
-import SimpleSchema from 'simpl-schema';
+import '../imports/api/users';
 
 Meteor.startup(() => {
-  // code to run on server at startup
-
-  // const petSchema = new SimpleSchema({
-  //    name: {
-  //        type: String,
-  //        min: 1,
-  //        max: 5,
-  //        optional: true
-  //    },
-  //    age:{
-  //        type: Number,
-  //        min: 0
-  //    },
-  //    contactNumber:{
-  //       type: String,
-  //       optional: true,
-  //       regEx: SimpleSchema.RegEx.Phone
-  //    }
-  // });
-  //
-  // petSchema.validate({
-  //     age: 21,
-  //     contactNumber: '1234'
-  //
-  // });
-
-  const EmployeeSchema = new SimpleSchema({
-      name:{
-          type: String,
-          min: 1,
-          max: 200
-      },
-      hourlyWage:{
-          type: Number,
-          min: 0
-      },
-      email:{
-          type: String,
-          regEx: SimpleSchema.RegEx.Email
-
-      }
-  });
-
-  EmployeeSchema.validate({
-     name: "Jacob John",
-     hourlyWage: 10,
-     email: "Jacob_John@dmail.com"
-  });
-
 
 
 });
